524df3164a9f5a105aaecbe1e66efd462493c1c3
wenhua <lshgdut@qq.com>
2025-07-27 13:00:14 +0800

    feat: remove chakra & add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 
-import { Provider } from "@/components/ui/provider"
-
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -30,7 +28,7 @@ export default function RootLayout({
       <body
         // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>{children}</Provider>
+        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-import Image from "next/image";
-import { Button, HStack } from "@chakra-ui/react"
+import { Button } from "@/components/ui/button"
+import { Loader2Icon } from "lucide-react"
 
-const Demo = () => {
-  return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
-  )
-}
+import SheetDemo from "@/demo/sheet"
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Demo />
+        <Button size="sm">
+          <Loader2Icon className="animate-spin" />
+          Please wait
+        </Button>
+        <SheetDemo />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         
